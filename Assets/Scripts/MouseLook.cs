0146f9710368ebd27bff71c71ee4eb382e6fc276
TxN <AimII@yandex.ru>
2014-07-22 15:25:42 +0400

    ScreenshotFix
    My first Git commit

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -36,6 +36,7 @@ public class MouseLook : MonoBehaviour {
 	}
 
 	IEnumerator ScreenCapture() {
+		#if !UNITY_WEBPLAYER
 		yield return new WaitForEndOfFrame ();
 		int width = Screen.width;
 		int height = Screen.height;
@@ -46,16 +47,24 @@ public class MouseLook : MonoBehaviour {
 
 		byte[] data = texture.EncodeToPNG();
 		Destroy (texture);
-		string filename = "C:\\Users\\Pavel\\YandexDisk-IslandNiggers\\Screenshots\\Pavel\\Screenshot" + System.DateTime.Now.ToString("MMddyyyy") + "_" + System.DateTime.Now.ToString("hhmmss") + ".png";//
+		string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal) + "\\Island\\";
+		if (!System.IO.Directory.Exists(path))
+		{
+			System.IO.Directory.CreateDirectory(path);
+		}
+
+		string filename =  path + "Screenshot" + System.DateTime.Now.ToString("MMddyyyy") + "_" + System.DateTime.Now.ToString("hhmmss") + ".png";//
 		int i=0;
 		while(System.IO.File.Exists(filename))
 		{
-			filename = "C:\\Users\\Pavel\\YandexDisk-IslandNiggers\\Screenshots\\Pavel\\Screenshot" + System.DateTime.Now.ToString("MMddyyyy") + " " + System.DateTime.Now.ToString("hhmmss") + " " + i + ".png";
+			filename =  path + "Screenshot" + System.DateTime.Now.ToString("MMddyyyy") + "_" + System.DateTime.Now.ToString("hhmmss") + "_" + i + ".png";
 			i++;
 		}
 		if(i==0){
-			filename = "C:\\Users\\Pavel\\YandexDisk-IslandNiggers\\Screenshots\\Pavel\\Screenshot" + System.DateTime.Now.ToString("MMddyyyy") + " " + System.DateTime.Now.ToString("hhmmss")+ ".png";
+			filename =  path + "Screenshot" + System.DateTime.Now.ToString("MMddyyyy") + "_" + System.DateTime.Now.ToString("hhmmss")+ ".png";
 		}
-		System.IO.File.WriteAllBytes (filename, data);
+			System.IO.File.WriteAllBytes (filename, data);
+
+		#endif
 	}
 }

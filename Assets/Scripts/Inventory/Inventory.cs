2c9264044cd1ae5a9afffe5991aeeae3a80f3ac8
TxN <AimII@yandex.ru>
2014-07-23 01:02:52 +0400

    Inventory
    Wow. Much bugs. Such glitchy. So broken.
    Now it seems not working at all.

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,73 @@
+﻿/// <summary>
+/// Inventory script. Manages player inventory
+/// </summary>
+using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour
+{	
+	public static int inventorySize = 127;
+	//public int[] itemQuantity = new int[inventorySize + 1];
+	public Item[] items = new Item[inventorySize + 1];	// Элементы инвентаря
+
+	public GameObject debugObj;
+
+
+	public int AddItem(GameObject obj)
+	{
+		int resIndex = -1;
+		Item it = obj.GetComponent<Item> ();
+		if (it.maxQ == 1) {
+			for (int i = 0; i < inventorySize; i++) {
+				if (!items [i]) {
+					// Итак, начинаем. Ибо простое присваивание запихает сюда только ссылку на объект, копируем все ручками
+					//items [i] = Item (it.name, it.title, it.desc, it.weight, it.maxQ, it.quantity, it.durability, it.maxDur, it.type);
+					items [i] = it;
+					items [i].name = it.name;
+
+					resIndex = i;
+					break;
+				}
+
+			}
+
+		} else { 				//TODO: добавить проверку на наполнение стака. Пока подождет
+			bool stacked = false;
+			for (int i = 0; i < inventorySize; i++) {
+				if (items [i]) {
+					if (it.name == items [i].name) {
+						items [i].quantity += it.quantity;
+						resIndex = i;
+						stacked = true;
+						break;
+					}
+				}
+			}
+
+			if (!stacked) {
+				for (int i = 0; i < inventorySize; i++) {
+
+					if (!items [i]) {
+						// Итак, начинаем. Ибо простое присваивание запихает сюда только ссылку на объект, копируем все ручками
+						//	items [i] = new Item (it.name, it.title, it.desc, it.weight, it.maxQ, it.quantity, it.durability, it.maxDur, it.type);
+						resIndex = i;
+						break;
+					}
+
+				}
+
+			}
+		}
+
+		return resIndex;
+	}
+
+			void Update()
+			{
+		if (Input.GetButtonDown("Use"))
+				{
+			AddItem (debugObj);
+				}
+			}
+
+}
---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour 
+
+{
+	public string name;
+	public string title;
+	public string desc;
+	public float weight;
+	public int maxQ;
+	public int quantity;
+	public int durability;
+	public int maxDur;
+	public enType type;
+	public enum enType
+	{
+		Weapon,
+		Instrument,
+		Food,
+		Seeds,
+		Resource,
+		Equipment
+	}
+
+
+	public Item(string n, string t, string d, float w, int mQ, int q, int dur, int mDur, enType typ)
+	{
+		name = n;
+		title = t;
+		desc = t;
+		weight = w;
+		maxQ = mQ;
+		quantity = q;
+		durability = dur;
+		maxDur = mDur;
+		type = typ;
+	}
+
+	public Item()
+	{
+		name = "DebugCube";
+		title = "DebugItem";
+		desc = "Just a placeholder";
+		weight = 0.01f;
+		maxQ = 1;
+		quantity = 1;
+		durability = 1;
+		maxDur = 1;
+		type = enType.Resource;
+	}
+
+}

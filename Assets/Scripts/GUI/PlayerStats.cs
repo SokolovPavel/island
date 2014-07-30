ae7f33b26568062a24f7b7b00467a2f9030865f1
SokolovPavel <sokolik777@sibmail.com>
2014-07-31 02:45:44 +0700

    Delete ExecuteInEditMode

--- Assets/Scripts/GUI/PlayerStats.cs
+++ Assets/Scripts/GUI/PlayerStats.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[ExecuteInEditMode]
 public class PlayerStats : MonoBehaviour {
 	bool sleeping;
 	public float health;
--- Assets/Scripts/GUI/simpleGuiScript.cs
+++ Assets/Scripts/GUI/simpleGuiScript.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[ExecuteInEditMode]
 public class simpleGuiScript : MonoBehaviour {
 
 	public Texture texture;

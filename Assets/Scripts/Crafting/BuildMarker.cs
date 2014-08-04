fb5c2166b0cccc33881c7ad58380aa7f7076de93
TxN <AimII@yandex.ru>
2014-08-04 19:34:05 +0400

    Minor additions and fixes
    Пофиксил пару проблемок, добавил новые :)

--- Assets/Scripts/Crafting/BuildMarker.cs
+++ Assets/Scripts/Crafting/BuildMarker.cs
@@ -4,9 +4,24 @@ using System.Collections;
 public class BuildMarker : MonoBehaviour {
 
 	public float rotSpd = 0.5f;
-	
+	public AudioClip placingSound;
+	public GameObject smokePuff;
+
+
 	// Update is called once per frame
 	void FixedUpdate () {
+
 		transform.rotation *= Quaternion.Euler(0,rotSpd,0);
+	}
+
+	void Start() {
+		smokePuff.SetActive (false);
+	}
+
+	public void Activate() { 
+		smokePuff.SetActive (true);
+		audio.PlayOneShot (placingSound);
+		Destroy (smokePuff, 2);
+
 	}
 }
--- Assets/Scripts/Crafting/Builder.cs
+++ Assets/Scripts/Crafting/Builder.cs
@@ -6,6 +6,7 @@ public class Builder : MonoBehaviour {
 	public Blueprint blueprint;
 	public bool built = false;
 	private GameObject buildMarker;
+	private bool mAct = false;
 	public MonoBehaviour mainBuildingScript;
 	// Use this for initialization
 	void Start () {
@@ -26,19 +27,26 @@ public class Builder : MonoBehaviour {
 		Inventory inv = user.GetComponent<Inventory> ();
 		InventoryGUI invGUI = user.GetComponent<InventoryGUI> ();
 		int index = invGUI.selectedIndex;
-
+		string outStr = "You need to add ";
 		for (int i = 0; i < blueprint.elements.Count; i++) {
 			if ((inv.items [index].name == blueprint.elements [i])&&(blueprint.quantities[i]>0)) {
+
 				if (blueprint.quantities [i] > inv.items [index].quantity) {
 
 					blueprint.quantities [i] -= inv.items [index].quantity;
 					inv.DestroyItem (index);
-					break;
+
+					//break;
 				} else {
 					inv.AddQuantity (index, -blueprint.quantities [i]);
 					blueprint.quantities [i] = 0;
 				}
+
+
 			}
+
+			outStr =outStr + blueprint.quantities [i];
+			outStr =outStr +"  " + blueprint.elements [i] + " ";
 		}
 		int q = 0;
 		for (int j = 0; j < blueprint.elements.Count; j++) {
@@ -47,16 +55,21 @@ public class Builder : MonoBehaviour {
 		}
 			Debug.Log (q);
 		if (q <= 0) {
+			outStr = "You successfully built the " + blueprint.name; 
 			built = true;
 			mainBuildingScript.enabled = true;
 			DestroyObject (this);
 			Destroy (buildMarker);
 		}
+		GameObject.FindGameObjectWithTag ("GameLogic").GetComponent<MessageBox> ().AddMessage (new GameMessage (outStr, GameMessage.messageType.ObjectMessage));
 
 	}
 
 	void Update() {
 		buildMarker.transform.position = transform.position;
-
+		if ((this.collider.enabled)&&(!mAct)) {
+			mAct = true;
+			buildMarker.GetComponent<BuildMarker> ().Activate ();
+		}
 	}
 }
